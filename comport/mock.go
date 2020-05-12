8fcc91622e042717e481ddb84d006e29558dcec2
binf1611@gmail.com <fpawel>
2020-05-12 08:32:05 +0300

    mock

---
+++ comport/mock.go
@@ -0,0 +1,43 @@
+package comport
+
+import (
+	"fmt"
+	"github.com/fpawel/comm"
+	"io"
+	"time"
+)
+
+func NewMock(f RequestToResponseFunc) comm.T {
+	return comm.New(NewMockPort(f), comm.Config{
+		TimeoutGetResponse: time.Millisecond,
+		TimeoutEndResponse: 0,
+	})
+}
+
+func NewMockPort(f RequestToResponseFunc) io.ReadWriter {
+	return &mockComport{f: f}
+}
+
+type RequestToResponseFunc = func(req []byte) []byte
+
+type mockComport struct {
+	req  []byte
+	resp []byte
+	f    RequestToResponseFunc
+}
+
+func (x *mockComport) Write(p []byte) (int, error) {
+	x.req = p
+	x.resp = x.f(x.req)
+	return len(p), nil
+}
+
+func (x *mockComport) Read(p []byte) (int, error) {
+	if len(x.resp) == 0 {
+		return 0, fmt.Errorf("unsupported request %02X", x.req)
+	}
+	if len(p) < len(x.resp) {
+		return len(x.resp), nil
+	}
+	return copy(p, x.resp), nil
+}
